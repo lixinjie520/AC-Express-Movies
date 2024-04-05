1d8fadda3b172c93ee853c951e183c893472df80
Amber Wang <wangshengyan6@gmail.com>
2024-04-05 12:37:57 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+//console.log(`express的內容為:${express} `);
 const app = express();
+//console.log(`app的內容為:${app} `);
 const port = 3000;
-
 app.get("/", (req, res) => {
-  res.send(`express app for movies`);
+  res.redirect("/movies");
 });
 
+app.get("/movies", (req, res) => {
+  res.send("listing movies");
+});
+
+app.get("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`reading movie: ${id}`);
+});
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port} `);
 });
