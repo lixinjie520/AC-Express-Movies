2192e1093a664af117f4ccaf31fda635500d5c4a
Amber Wang <wangshengyan6@gmail.com>
2024-04-05 13:54:19 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const express = require("express");
 const app = express();
 //console.log(`app的內容為:${app} `);
 const port = 3000;
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.redirect("/movies");
+    res.redirect("/movies");
 });
 
 app.get("/movies", (req, res) => {
-  res.send("listing movies");
+    res.send("listing movies");
 });
 
 app.get("/movie/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`reading movie: ${id}`);
+    const id = req.params.id;
+    res.send(`reading movie: ${id}`);
 });
 app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port} `);
+    console.log(`express server is running on http://localhost:${port} `);
 });
