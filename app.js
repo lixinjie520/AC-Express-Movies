6498f1ab4bb0e69329ce370ff8133d0d1b79d1a0
Amber Wang <wangshengyan6@gmail.com>
2024-04-10 11:15:17 +0800

    Chore: add some comment to help me figure out some express methods

--- app.js
+++ app.js
@@ -8,40 +8,44 @@ const movies = require("./public/jsons/movies.json").results;
 const port = 3000;
 const BASE_IMG_URL = "https://movie-list.alphacamp.io/posters/";
 // 載入模組（例如 Express、Handlebars)、設定路由
+// 設定模版引擎
 app.engine(".hbs", engine({ extname: ".hbs" }));
+// 使用模版引擎是hbs
 app.set("view engine", ".hbs");
+// 設定模版引擎的路徑
 app.set("views", "./views");
 
 // 載入靜態資料夾
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.redirect("/movies");
+  res.redirect("/movies");
 });
 
 app.get("/movies", (req, res) => {
-    // 加上trim()就不行!!!
-    const keyword = req.query.search;
-    //console.log(keyword);
-    const matchedMovies = keyword ?
-        movies.filter((mv) =>
-            Object.values(mv).some((property) => {
-                if (typeof property === "string") {
-                    return property.toLowerCase().includes(keyword.toLowerCase());
-                }
-                return false;
-            })
-        ) :
-        movies;
-    res.render("index", { movies: matchedMovies, BASE_IMG_URL, keyword });
+  // 取得使用者搜尋的電影名稱
+  // 加上trim()就不行!!!
+  const keyword = req.query.search;
+  //console.log(keyword);
+  const matchedMovies = keyword
+    ? movies.filter((mv) =>
+        Object.values(mv).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          }
+          return false;
+        })
+      )
+    : movies;
+  res.render("index", { movies: matchedMovies, BASE_IMG_URL, keyword });
 });
 
 app.get("/movie/:id", (req, res) => {
-    // 取得 id
-    const id = req.params.id;
-    const movie = movies.find((mv) => mv.id.toString() === id);
-    res.render("detail", { movie, BASE_IMG_URL });
+  // 取得 id
+  const id = req.params.id;
+  const movie = movies.find((mv) => mv.id.toString() === id);
+  res.render("detail", { movie, BASE_IMG_URL });
 });
 app.listen(port, () => {
-    console.log(`express server is running on http://localhost:${port} `);
+  console.log(`express server is running on http://localhost:${port} `);
 });
